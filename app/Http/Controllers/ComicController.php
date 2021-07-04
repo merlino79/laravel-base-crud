ea860328506339a7f83befb40e314fa7a1dffd19
Merlino79 <alessandro.ciancio@hotmail.com>
2021-07-04 12:16:14 +0200

    coorezione nella pagina error

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -67,9 +67,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Comic $comic)
+    public function show($id)
     {   
-        //$comic = Comic::find($id);
+        $comic = Comic::find($id);
         // dd($comic);
         if($comic){
             return view('comics.show', compact('comic'));
